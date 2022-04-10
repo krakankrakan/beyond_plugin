dcd8049c34561365bce1cff02201ab9cb30d95b8
Frederik Pustelnik <fpustelnik@web.de>
2022-04-10 02:51:53 +0200

    Conditionals

--- llil.py
+++ llil.py
@@ -75,6 +75,106 @@ def lift_bn_xor(il, inst):
         [inst.operand[0].register, inst.operand[1].register, inst.operand[2].register])
 
 #
+# Conditional Expressions
+#
+
+def cond(il, inst, expr, src):
+    return il.set_flag(
+        "F",
+        expr(
+            src
+        )
+    )
+
+def lift_bn_sfeqi(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_equal(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].immediate])
+
+def lift_bn_sfnei(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_not_equal(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].immediate])
+
+def lift_bn_sfgesi(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_signed_greater_equal(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].immediate])
+
+def lift_bn_sfgeui(il, inst):
+    return lift_bn_sfgesi(il, inst)
+
+def lift_bn_sfgtsi(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_signed_greater_than(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].immediate])
+
+def lift_bn_sfgtui(il, inst):
+    return lift_bn_sfgesi(il, inst)
+
+def lift_bn_sflesi(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_signed_less_equal(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].immediate])
+
+def lift_bn_sfleui(il, inst):
+    return lift_bn_sfgesi(il, inst)
+
+def lift_bn_sfltsi(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_signed_less_than(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].immediate])
+
+def lift_bn_sfltui(il, inst):
+    return lift_bn_sfgesi(il, inst)
+
+def lift_bn_sfeq(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_equal(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].register])
+
+def lift_bn_sfne(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_not_equal(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].register])
+
+def lift_bn_sfges(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_signed_greater_equal(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].register])
+
+def lift_bn_sfgeu(il, inst):
+    return lift_bn_sfgesi(il, inst)
+
+def lift_bn_sfgts(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_signed_greater_than(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].register])
+
+def lift_bn_sfgtu(il, inst):
+    return lift_bn_sfgesi(il, inst)
+
+# TODO: Don't need this function
+def lift_bn_sfles(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_signed_less_equal(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].register])
+
+# TODO: Don't need this function
+def lift_bn_sfleu(il, inst):
+    return lift_bn_sfgesi(il, inst)
+
+# TODO: Don't need this function
+def lift_bn_sflts(il, inst):
+    return cond(il, inst,
+        lambda src : il.compare_signed_less_than(4, op_as_il(il, src[0]), op_as_il(il, src[1]))
+        [inst.operand[0].register, inst.operand[1].register])
+
+# TODO: Don't need this function
+def lift_bn_sfltu(il, inst):
+    return lift_bn_sfgesi(il, inst)
+
+#
 # Control Flow Expressions
 #
 
